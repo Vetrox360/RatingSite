1139abd3d6ca96e530496b487631e92b5e659415
Vetrox360 <94619555+Vetrox360@users.noreply.github.com>
2024-03-09 16:36:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,26 +5,21 @@ document.addEventListener("DOMContentLoaded", async function () {
     const user = await app.logIn(credentials);
     const mongodb = app.currentUser.mongoClient("mongodb-atlas");
     const collection = mongodb.db("rating-data").collection("data");
-
     document
       .getElementById("picture-form")
       .addEventListener("submit", async function (e) {
         e.preventDefault();
         const personName = document.getElementById("person-name").value;
         const pictureFile = document.getElementById("picture-file").files[0];
-        const pictureUrl = document.getElementById("picture-url").value;
         const pictureRating = document.getElementById("picture-rating").value;
         const pictureDescription = document.getElementById(
           "picture-description"
         ).value;
-
         const pictureData = {
           personName: personName,
-          pictureUrl: pictureUrl,
           pictureRating: pictureRating,
           pictureDescription: pictureDescription,
         };
-
         if (pictureFile) {
           const reader = new FileReader();
           reader.readAsDataURL(pictureFile);
@@ -32,78 +27,170 @@ document.addEventListener("DOMContentLoaded", async function () {
             pictureData.pictureFile = reader.result;
             await collection.insertOne(pictureData);
             refreshPictureFeed();
+            removePreview();
           };
         } else {
           await collection.insertOne(pictureData);
           refreshPictureFeed();
         }
-
         document.getElementById("picture-form").reset();
       });
-
     async function refreshPictureFeed() {
-      const pictures = await collection.find({});
-      const pictureFeed = document.querySelector("#picture-feed tbody");
+      const pictures = await collection.find(
+        {},
+        {
+          projection: {
+            _id: 1,
+            personName: 1,
+            pictureRating: 1,
+            pictureDescription: 1,
+            pictureFile: 1,
+            pictureUrl: 1,
+          },
+        }
+      );
+      const pictureFeed = document.querySelector("#picture-feed");
       pictureFeed.innerHTML = "";
-
       pictures.forEach((picture) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-          <td><img src="${
-            picture.pictureFile || picture.pictureUrl
-          }" alt="Picture"></td>
-          <td>${picture.personName}</td>
-          <td>${picture.pictureRating}</td>
-          <td>${picture.pictureDescription}</td>
-        `;
-        pictureFeed.appendChild(row);
+        const pictureDiv = document.createElement("div");
+        pictureDiv.classList.add("picture-item");
+        pictureDiv.setAttribute(
+          "data-src",
+          picture.pictureFile || picture.pictureUrl
+        );
+        pictureDiv.innerHTML = `
+  <div class="placeholder-image"></div>
+  <div class="rating">${picture.pictureRating}</div>
+  <div class="content">
+    <div class="details">
+      <h3>${picture.personName}</h3>
+      <p class="description">${picture.pictureDescription}</p>
+    </div>
+  </div>
+  <button class="delete-button" data-id="${picture._id}">
+    <img src="./trash.svg" alt="delete" id="delete-img" />
+  </button>
+`;
+        pictureFeed.appendChild(pictureDiv);
+      });
+      const lazyLoadImages = document.querySelectorAll(".picture-item");
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const pictureDiv = entry.target;
+            const imageSrc = pictureDiv.getAttribute("data-src");
+            const placeholderImage =
+              pictureDiv.querySelector(".placeholder-image");
+            const lowResImage = new Image();
+            lowResImage.src = `${imageSrc}?quality=10`; // Adjust the quality parameter as needed
+            lowResImage.onload = () => {
+              placeholderImage.style.backgroundImage = `url(${lowResImage.src})`;
+            };
+            const highResImage = new Image();
+            highResImage.src = imageSrc;
+            highResImage.onload = () => {
+              pictureDiv.style.backgroundImage = `url(${highResImage.src})`;
+              placeholderImage.style.display = "none";
+            };
+            highResImage.onerror = () => {
+              placeholderImage.style.backgroundImage =
+                "url(path/to/fallback-image.jpg)";
+            };
+            observer.unobserve(pictureDiv);
+          }
+        });
+      });
+      lazyLoadImages.forEach((image) => {
+        observer.observe(image);
+      });
+      pictureFeed.addEventListener("click", async function (e) {
+        if (e.target.closest(".delete-button")) {
+          const deleteButton = e.target.closest(".delete-button");
+          const pictureId = deleteButton.getAttribute("data-id");
+          await collection.deleteOne({
+            _id: new Realm.BSON.ObjectId(pictureId),
+          });
+          refreshPictureFeed();
+        }
       });
     }
-
     refreshPictureFeed();
   } catch (error) {
     console.error("Error:", error);
   }
 });
-
 document.getElementById("search-input").addEventListener("input", function () {
   var searchValue = this.value.toLowerCase();
-  var rows = document.querySelectorAll("#picture-feed tbody tr");
-  rows.forEach(function (row) {
-    var personName = row.cells[1].textContent.toLowerCase();
+  var pictureItems = document.querySelectorAll("#picture-feed .picture-item");
+  pictureItems.forEach(function (pictureItem) {
+    var personName = pictureItem.querySelector("h3").textContent.toLowerCase();
     if (personName.includes(searchValue)) {
-      row.style.display = "";
+      pictureItem.style.display = "";
     } else {
-      row.style.display = "none";
+      pictureItem.style.display = "none";
     }
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const passwordModal = document.getElementById("password-modal");
-  const passwordInput = document.getElementById("password-input");
-  const submitPasswordButton = document.getElementById("submit-password");
-
-  const predefinedPassword = "roksia123";
-
-  passwordModal.style.display = "block";
-
-  submitPasswordButton.addEventListener("click", function () {
-    const enteredPassword = passwordInput.value;
-    if (enteredPassword === predefinedPassword) {
-      passwordModal.style.display = "none";
-    } else {
-      alert("Incorrect password. Please try again.");
-      passwordInput.value = "";
-    }
-  });
-});
-
-const themeToggle = document.getElementById('theme-toggle');
-
-themeToggle.addEventListener('click', () => {
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", () => {
   const body = document.body;
-  body.classList.toggle('light-mode'); // Add/remove 'light-mode' class
+  body.classList.toggle("light-mode");
 });
+const fileDropZone = document.querySelector(".file-drop-zone");
+const fileInput = document.getElementById("picture-file");
+const filePreview = document.querySelector(".file-preview");
+const dropZoneText = document.getElementById("drop-zone-text");
+fileDropZone.addEventListener("click", () => {
+  fileInput.click();
+});
+fileDropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  fileDropZone.classList.add("dragover");
+});
+fileDropZone.addEventListener("dragleave", () => {
+  fileDropZone.classList.remove("dragover");
+});
+fileDropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  fileDropZone.classList.remove("dragover");
+  const files = e.dataTransfer.files;
+  if (files.length > 0) {
+    fileInput.files = files;
+    showPreview(files[0]);
+  }
+});
+fileInput.addEventListener("change", () => {
+  const files = fileInput.files;
+  if (files.length > 0) {
+    showPreview(files[0]);
+  }
+});
+function showPreview(file) {
+  const reader = new FileReader();
+  reader.onload = () => {
+    const previewImage = document.createElement("img");
+    previewImage.src = reader.result;
+    filePreview.innerHTML = "";
+    filePreview.appendChild(previewImage);
+    dropZoneText.style.display = "none";
+  };
+  reader.readAsDataURL(file);
+}
+function removePreview() {
+  filePreview.innerHTML = "";
+  dropZoneText.style.display = "block";
+  fileInput.value = "";
+}
+// Add this event listener for the delete button click event
+document.querySelector(".forms").addEventListener("click", function (e) {
+  if (e.target.closest(".reset")) {
+    removePreview();
+  }
+});
+const searchButton = document.querySelector('.searchb');
+const searchInput = document.querySelector('.search');
 
+searchButton.addEventListener('click', () => {
+  searchInput.focus();
+});
 
